7e2ce782090f32e296de3b82dc0801a61bb8f1ff
hunterb2121 <hunterb01@outlook.com>
2024-01-20 11:57:24 -0800

    commit for beginning of project and development configurations

---
+++ app.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+app.config.from_pyfile("config.py")
---
+++ config.py
@@ -0,0 +1,3 @@
+DEBUG = True
+MAIL_FROM_EMAIL = "libraryapp@gmail.com"
+SECRET_KEY = "Jhsi_83947hiuhe??@#!jfiankJHIPUJjdai7451654i*&#ENWIY*#$JRHNG87y94iouhwsi__-uhfe"
